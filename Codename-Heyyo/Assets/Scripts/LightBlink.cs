f78f22ea7e094e9c1cbc071b9dbf19cf00f26298
Neo <prestonmccubbin@gmail.com>
2020-03-14 01:07:40 -0400

    Light Plan
    Plan for lighting and design

---
+++ Codename-Heyyo/Assets/Scripts/LightBlink.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LightBlink : MonoBehaviour
+{
+    private Animation anim;
+
+    int times = 4;
+
+    void Start()
+    {
+        anim = gameObject.GetComponent<Animation>();
+        StartCoroutine(LightBlinkTime());
+
+    }
+
+
+    IEnumerator LightBlinkTime()
+    {
+        yield return new WaitForSeconds(2f);
+
+        while (times > 0) 
+        {
+            if (times <= 2)
+            {
+                while (times > 0)
+                {
+                    anim.Play("New Animation2");
+                    times--;
+                    yield return new WaitForSeconds(6f);
+                }
+
+            }
+            else 
+            {
+                anim.Play("New Animation");
+                times--;
+                yield return new WaitForSeconds(6f);
+
+            }
+            
+        }
+    }
+}
---
+++ Codename-Heyyo/Assets/Scripts/PlayerMov.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerMov : MonoBehaviour
+{
+    public float moveSpeed;
+    public float maxSpeeed;
+    private Vector3 input;
+    private Vector3 spawn;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        input = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+        if (GetComponent<Rigidbody>().velocity.magnitude < maxSpeeed)
+        {
+
+            GetComponent<Rigidbody>().AddForce(input * moveSpeed);
+
+        }
+        else 
+        {
+            GetComponent<Rigidbody>().AddForce(input / moveSpeed);
+
+        }
+
+    }
+}
