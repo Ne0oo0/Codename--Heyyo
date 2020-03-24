05616880f4e96bdffe3b6767c9696ac1ec782fe8
Neo <prestonmccubbin@gmail.com>
2020-03-23 21:57:43 -0400

    Setup area formovement
    Set up area for movement testing with ramp

--- Codename-Heyyo/Assets/Scripts/PlayerMov.cs
+++ Codename-Heyyo/Assets/Scripts/PlayerMov.cs
@@ -4,10 +4,6 @@ using UnityEngine.SceneManagement;
 
 public class PlayerMov : MonoBehaviour
 {
-    public float moveSpeed;
-    public float maxSpeeed;
-    private Vector3 input;
-    private Vector3 spawn;
 
 
     // Start is called before the first frame update
@@ -19,18 +15,6 @@ public class PlayerMov : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        input = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
-        if (GetComponent<Rigidbody>().velocity.magnitude < maxSpeeed)
-        {
-
-            GetComponent<Rigidbody>().AddForce(input * moveSpeed);
-
-        }
-        else 
-        {
-            GetComponent<Rigidbody>().AddForce(input / moveSpeed);
-
-        }
 
     }
 }
